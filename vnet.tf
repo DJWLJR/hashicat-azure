39891a6762350d0bf19967ea23386478607359b9
David Lauridsen <david@gryphic.com>
2020-08-26 18:30:03 +0000

    Updated vnet code

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,7 @@
 module "network" {
-  source  = "app.terraform.io/davidlauridsen-training/network/azurerm"
+  source              = "app.terraform.io/davidlauridsen-training/network/azurerm"
   resource_group_name = azurerm_resource_group.test.name
-  version = "3.0.1"
-  location = "West Europe"
+  version             = "3.0.1"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
