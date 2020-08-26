70e693729808f84ca7c2f4bc818fff5bd33d9804
David Lauridsen <david@gryphic.com>
2020-08-26 18:27:17 +0000

    Updated vnet using module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,18 @@
 module "network" {
   source  = "app.terraform.io/davidlauridsen-training/network/azurerm"
+  resource_group_name = azurerm_resource_group.test.name
   version = "3.0.1"
-  # insert required variables here
-  location = "East US"
+  location = "West Europe"
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "my-resources"
+  location = "West Europe"
 }
